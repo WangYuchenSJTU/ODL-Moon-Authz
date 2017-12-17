786a060b2396dc2d6ecdf429d5deca51234715a2
Wang Yuchen <31466770+WangYuchenSJTU@users.noreply.github.com>
2017-12-17 18:23:35 +0800

    Update MoonAuthorizationFilter.java

--- aaa/aaa-shiro/impl/src/main/java/org/opendaylight/aaa/impl/shiro/filters/MoonAuthorizationFilter.java
+++ aaa/aaa-shiro/impl/src/main/java/org/opendaylight/aaa/impl/shiro/filters/MoonAuthorizationFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2017 Orange and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -19,14 +19,6 @@ import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.authz.AuthorizationFilter;
 import org.opendaylight.aaa.impl.AAAShiroProvider;
 import org.opendaylight.aaa.impl.shiro.principal.ODLPrincipalImpl;
-import org.opendaylight.controller.md.sal.binding.api.DataBroker;
-import org.opendaylight.controller.md.sal.binding.api.ReadOnlyTransaction;
-import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
-import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.aaa.rev161214.HttpAuthorization;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.aaa.rev161214.http.authorization.Policies;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.aaa.rev161214.http.permission.Permissions;
-import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.sun.jersey.api.client.Client;
@@ -40,31 +32,15 @@ import org.json.*;
 import java.io.*;
 
 /**
- * Provides a dynamic authorization mechanism for restful web services with permission grain
- * scope.  <code>aaa.yang</code> defines the model for this filtering mechanism.
- * This model exposes the ability to manipulate policy information for specific paths
- * based on a tuple of (role, http_permission_list).
+ * Provides a dynamic authorization mechanism for restful web services from Moon platform
  *
  * This mechanism will only work when put behind <code>authcBasic</code>
  */
 public class MoonAuthorizationFilter extends AuthorizationFilter {
 
-    private String RestServerURL = "http://localhost:8081/shiroservlet-1.0-SNAPSHOT/ShiroServlet";
+    private String moonServerURL = "http://localhost:31003";
 
     private static final Logger LOG = LoggerFactory.getLogger(MoonAuthorizationFilter.class);
-
-    private static final InstanceIdentifier<HttpAuthorization> AUTHZ_CONTAINER_IID =
-            InstanceIdentifier.builder(HttpAuthorization.class).build();
-
-    public static Optional<HttpAuthorization> getHttpAuthzContainer(final DataBroker dataBroker)
-            throws ExecutionException, InterruptedException, ReadFailedException {
-
-        try (ReadOnlyTransaction ro = dataBroker.newReadOnlyTransaction()) {
-            final CheckedFuture<Optional<HttpAuthorization>, ReadFailedException> result =
-                    ro.read(LogicalDatastoreType.CONFIGURATION, AUTHZ_CONTAINER_IID);
-            return result.get();
-        }
-    }
 
     @Override
     public boolean isAccessAllowed(final ServletRequest request, final ServletResponse response,
@@ -80,23 +56,32 @@ public class MoonAuthorizationFilter extends AuthorizationFilter {
     }
 
     private boolean RestAuthorization(final String username, final String requestURI, final String method) {
-        final JSONTokener tokener;
-        final JSONObject object;
-        final String input = "{\"method\":\"authz\",\"username\":\""+ username + "\",\"uri\":\"" + requestURI + "\",\"action\":\"" + method + "\"}";
+        final String fullUrl = String.format("%s/interface/authz/deny/project_id/%s/object_name/%s", moonServerURL, username, method);
+        //final String fullUrl = String.format("%s/interface/authz/grant/project_id/%s/object_name/%s", moonServerURL, username, method);
         final ClientConfig config = new DefaultClientConfig();
         final Client client = Client.create(config);
-        final WebResource webResource = client.resource(RestServerURL);
-        final ClientResponse response = webResource.type("application/json").post(ClientResponse.class, input);
+        final WebResource webResource = client.resource(fullUrl);
+        final ClientResponse response = webResource.type("application/json").get(ClientResponse.class);
         final String output = response.getEntity(String.class);
-        tokener = new JSONTokener(output);
-        object = new JSONObject(tokener);
-	String result;
+        final JSONTokener tokener = new JSONTokener(output);
+        final JSONObject object = new JSONObject(tokener);
+
+	boolean result;
         try {
-            result = (String) object.get("response");
+            result = (Boolean) object.get("result");
         } catch (NullPointerException e){
             return false;
         }
-        if (result.equals("true"))return true;
-        else return false;
+        return result;
+    }
+
+     /**
+     * The URL of the Moon server. Injected from
+     * <code>aaa-app-config.xml</code>.
+     *
+     * @param url the URL specified in <code>aaa-app-config.xml</code>.
+     */
+    public void setUrl(final String url) {
+        moonServerURL = url;
     }
 }
